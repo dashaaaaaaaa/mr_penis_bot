ab2a4aa3d15e8e3e1e6f3398b59ee13b5da9fcd8
dashaaaaaaaa <dolha03@gmail.com>
2025-03-09 01:12:06 +0300

    код обнова

--- mr_penis_bot.py
+++ mr_penis_bot.py
@@ -9,7 +9,7 @@ from aiogram.enums import ChatType
 TOKEN = "7720767705:AAGCz87dzusbLM1zrKMLh04jvnfHSwnloKE"
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(bot)
 
 # Список рандомных сообщений
 random_messages = [
